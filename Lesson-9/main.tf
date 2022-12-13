9cd19f3685036dba9300d05b053d6a7840077437
Anvar Kalykov <anvariok@gmail.com>
2022-12-14 00:10:59 +0600

    Added Lesson-9 Data Sources AMI ID

---
+++ Lesson-9/main.tf
@@ -0,0 +1,51 @@
+
+
+#----------------------------------------------------------
+# My Terraform
+#
+# Find Latest AMI id of:
+#    - Ubuntu 22.04
+#    - Amazon Linux 2
+#    - Windows Server 2016 Base
+#
+# Made by Anvar
+#-----------------------------------------------------------
+
+
+provider "aws" {
+
+}
+
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+data "aws_ami" "latest_amazon" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+}
+
+output "latest_amazon_linux_ami_id" {
+  value = data.aws_ami.latest_amazon.id
+}
+
+output "latest_amazon_linux_ami_name" {
+  value = data.aws_ami.latest_amazon.name
+}
+
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.latest_ubuntu.id
+}
+
+output "latest_ubuntu_ami_name" {
+  value = data.aws_ami.latest_ubuntu.name
+}

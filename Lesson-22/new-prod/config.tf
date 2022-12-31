a8e1c65830c2b60b7650b78ec8d610f4ceead7f7
Anvar Kalykov <anvariok@gmail.com>
2022-12-31 15:24:20 +0600

    Terraform state

---
+++ Lesson-22/new-prod/config.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2" // Region where to Create Resources
+}
+
+terraform {
+  backend "s3" {
+    bucket = "lambda.create.me.on-1662893034.3455179"    // Bucket where to SAVE Terraform State
+    key    = "new-prod/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    region = "eu-west-1"                 // Region where bucket is created
+  }
+}
